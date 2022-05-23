c0c8363676d1b12d081472460e0d18321e5d6f1b
denispankove <denispankove@gmail.com>
2022-05-23 09:44:45 +0300

    added thumbnail generation

--- download-manager/attachment_service.py
+++ download-manager/attachment_service.py
@@ -1,8 +1,11 @@
+import datetime
 from minio import Minio
 import sys
 import os
 import requests
 import base64
+from datetime import datetime
+from PIL import Image
 
 
 class AttachmentService:
@@ -12,17 +15,20 @@ class AttachmentService:
         self.minio_access_key = minio_access_key
         self.minio_secret_key = minio_secret_key
         self.url = url
-        self.ext = ext
+        self.ext = ext.lower()
         self.project_name = project_name
         self.token = token
         self.authorization = str(base64.b64encode(bytes(':' + self.token, 'ascii')), 'ascii')
         self.headers = {
           'Authorization': 'Basic '+self.authorization
         }
+        self.image_extensions = [".jpg", ".jpeg", ".png", ".ico", ".im", ".pcx", ".tga", ".tiff"]
 
     async def process_attachment(self):
         file_name = self.url.split("/")[-1] + self.ext
-        file_bucket_path = f"{self.project_name}/{file_name}"
+        time_now = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        file_bucket_path = f"{self.project_name}/{time_now}/{file_name}"
+        value = {}
         azure_response = requests.get(url=self.url+"?download=true", headers=self.headers)
         open(file_name, 'wb').write(azure_response.content)
         if os.stat(file_name).st_size == 0:
@@ -30,21 +36,39 @@ class AttachmentService:
             full_download_path = f"http://{self.minio_end_point}/attachments/assets/bad-attachment.png"
             file_name = "bad-attachment.png"
         else:
-            full_download_path = f"http://{self.minio_end_point}/{self.bucket_name}/{file_bucket_path}"
             client = Minio(
                 self.minio_end_point,
                 access_key=self.minio_access_key,
                 secret_key=self.minio_secret_key,
                 secure=False,
             )
+            full_download_path = f"http://{self.minio_end_point}/{self.bucket_name}/{file_bucket_path}"
             client.fput_object(
                 self.bucket_name, file_bucket_path, file_name,
             )
+            if self.ext in self.image_extensions:
+                image = Image.open(file_name)
+                thumbnail_name = self.url.split("/")[-1] + "-thumbnail" + self.ext
+                thumbnail_file_path = f"{self.project_name}/{time_now}/{thumbnail_name}"
+                thumbnail_image = image.resize((256, 256))
+                thumbnail_image.save(thumbnail_name)
+                client.fput_object(
+                    self.bucket_name, thumbnail_file_path, thumbnail_name,
+                )
+                thumbnail_path = f"http://{self.minio_end_point}/{self.bucket_name}/{thumbnail_file_path}"
+                os.remove(thumbnail_name)
+                value = {
+                    "attachmentPath": full_download_path,
+                    "fileName": file_name,
+                    "thumbnailPath": thumbnail_path,
+                    "thumbnailName": thumbnail_name
+                    }
+            else:
+                value = {
+                    "attachmentPath": full_download_path,
+                    "fileName": file_name
+                    }
             os.remove(file_name)
-        value = {
-            "attachmentPath": full_download_path,
-            "fileName": file_name
-        }
         print(value)
         sys.stdout.flush()
         return value
